9b1528348644038884eac235f446609181993b02
Luiz Mário L. Pascoal <luizmlpascoal@gmail.com>
2022-05-27 01:52:37 -0300

    organize layers

--- src/server/utils/descriptorBuilder.js
+++ src/server/utils/descriptorBuilder.js
@@ -84,12 +84,10 @@ module.exports = function (app) {
     Internal.getGroupsOrder = function () {
 
         return [
-            'pasture',
+            'agrotoxicos',
+            'agropecuaria',
             'areas_especiais',
             'areas_declaradas',
-            'agropecuaria',
-            'campo',
-            'inspecao_visual',
             'infraestrutura',
             'imagens'
         ]
