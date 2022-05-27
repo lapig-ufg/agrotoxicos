73b53474aacc3af03fed9995fe4118bf1bcc053e
Gabriel <biel_srp@hotmail.com>
2022-05-27 10:58:06 -0300

    environment tst

--- src/client/src/environments/environment.prod.ts
+++ src/client/src/environments/environment.prod.ts
@@ -6,19 +6,13 @@ export const environment = {
   APP_NAME: 'Agrotoxicos',
   production: false,
   GTAG: 'UA-215301850-1',
-  // OWS_API: 'https://ows.lapig.iesa.ufg.br/api',
-  // OWS: 'https://ows.lapig.iesa.ufg.br',
-  // OWS_O1: "https://o1.lapig.iesa.ufg.br/ows",
-  // OWS_O2: "https://o2.lapig.iesa.ufg.br/ows",
-  // OWS_O3: "https://o3.lapig.iesa.ufg.br/ows",
-  // OWS_O4: "https://o4.lapig.iesa.ufg.br/ows",
 
-  OWS_API: 'https://ows-homolog.lapig.iesa.ufg.br/api',
-  OWS: 'https://ows-homolog.lapig.iesa.ufg.br',
-  OWS_O1: "https://o6.lapig.iesa.ufg.br/ows",
-  OWS_O2: "https://o7.lapig.iesa.ufg.br/ows",
-  OWS_O3: "https://o8.lapig.iesa.ufg.br/ows",
-  OWS_O4: "https://o5.lapig.iesa.ufg.br/ows",
+   OWS_API: 'https://ows.lapig.iesa.ufg.br/api',
+   OWS: 'https://ows.lapig.iesa.ufg.br',
+   OWS_O1: "https://o1.lapig.iesa.ufg.br/ows",
+   OWS_O2: "https://o2.lapig.iesa.ufg.br/ows",
+   OWS_O3: "https://o3.lapig.iesa.ufg.br/ows",
+   OWS_O4: "https://o4.lapig.iesa.ufg.br/ows",
   
   APP_URL: 'https://agrotoxicosdev.lapig.iesa.ufg.br'
 };
