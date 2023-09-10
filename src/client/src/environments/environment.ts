57df760701aa65ec701bd0862ba66d2b5ae13f91
Jairo Matos da Rochal <devjairomr@gmail.com>
2023-09-10 20:23:35 -0300

    dowload-minio

--- src/client/src/environments/environment.ts
+++ src/client/src/environments/environment.ts
@@ -15,11 +15,12 @@ export const environment = {
 
   OWS_API: 'https://ows-homolog.lapig.iesa.ufg.br/api',
   OWS: 'https://ows-homolog.lapig.iesa.ufg.br',
+  LAPIG_DOWNLOAD_API: 'https://download.lapig.iesa.ufg.br',
   OWS_O1: "https://o6.lapig.iesa.ufg.br/ows",
   OWS_O2: "https://o7.lapig.iesa.ufg.br/ows",
   OWS_O3: "https://o8.lapig.iesa.ufg.br/ows",
   OWS_O4: "https://o5.lapig.iesa.ufg.br/ows",
-  
+
 
   APP_URL: 'https://agrotoxicosdev.lapig.iesa.ufg.br'
 };
