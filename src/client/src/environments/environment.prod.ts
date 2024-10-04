2ae07c35976acb5fe8b5e4e25d8e91b0be4a77cb
Jairo Matos Da Rocha <jairo@geocodelabs.com>
2024-10-03 21:45:43 -0300

    new server

--- src/client/src/environments/environment.prod.ts
+++ src/client/src/environments/environment.prod.ts
@@ -9,12 +9,12 @@ export const environment = {
 
   OWS_API: 'https://ows.lapig.iesa.ufg.br/api',
   OWS: 'https://ows.lapig.iesa.ufg.br',
-  OWS_O1: "https://o6.lapig.iesa.ufg.br/ows",
-  OWS_O2: "https://o7.lapig.iesa.ufg.br/ows",
-  OWS_O3: "https://o8.lapig.iesa.ufg.br/ows",
-  OWS_O4: "https://o5.lapig.iesa.ufg.br/ows",
+  OWS_O1: "https://o1.lapig.iesa.ufg.br/ows",
+  OWS_O2: "https://o2.lapig.iesa.ufg.br/ows",
+  OWS_O3: "https://o3.lapig.iesa.ufg.br/ows",
+  OWS_O4: "https://o4.lapig.iesa.ufg.br/ows",
   LAPIG_DOWNLOAD_API: 'https://download.lapig.iesa.ufg.br',
-  APP_URL: 'https://agrotoxicosdev.lapig.iesa.ufg.br'
+  APP_URL: 'https://agrotoxicos.lapig.iesa.ufg.br'
 };
 
 /*
--- src/client/src/environments/environment.ts
+++ src/client/src/environments/environment.ts
@@ -16,11 +16,11 @@ export const environment = {
   OWS_API: 'https://ows-homolog.lapig.iesa.ufg.br/api',
   OWS: 'https://ows-homolog.lapig.iesa.ufg.br',
   LAPIG_DOWNLOAD_API: 'https://download.lapig.iesa.ufg.br',
-  OWS_O1: "https://o6.lapig.iesa.ufg.br/ows",
-  OWS_O2: "https://o7.lapig.iesa.ufg.br/ows",
-  OWS_O3: "https://o8.lapig.iesa.ufg.br/ows",
-  OWS_O4: "https://o5.lapig.iesa.ufg.br/ows",
+  OWS_O1: "https://o1.lapig.iesa.ufg.br/ows",
+  OWS_O2: "https://o2.lapig.iesa.ufg.br/ows",
+  OWS_O3: "https://o3.lapig.iesa.ufg.br/ows",
+  OWS_O4: "https://o4.lapig.iesa.ufg.br/ows",
 
 
-  APP_URL: 'https://agrotoxicosdev.lapig.iesa.ufg.br'
+  APP_URL: 'https://agrotoxicos.lapig.iesa.ufg.br'
 };
