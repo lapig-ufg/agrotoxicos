bffad4d138bffef0c783d639f2a7c0edf1576cd8
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2026-02-07 20:59:12 -0300

    new sistem

--- src/server/app-cluster.js
+++ src/server/app-cluster.js
@@ -1,27 +1,37 @@
 require('dotenv').config();
 const cluster = require('cluster');
+const os = require('os');
 
-let numCPUs = 48;
+const totalCPUs = os.cpus().length;
+let clusterWorkerSize = totalCPUs;
 
-if (cluster.isMaster) {
-
-    if (process.env.NODE_ENV == 'dev') {
-        numCPUs = 2
-    }
-
-    for (let i = 0; i < numCPUs; i++) {
-        let ENV_VAR = {}
-        if (i == 0) {
-            ENV_VAR = { 'PRIMARY_WORKER': 1 }
+if (clusterWorkerSize > 1) {
+    if (cluster.isMaster) {
+        if (process.env.NODE_ENV === 'dev') {
+            clusterWorkerSize = 2;
+        } else if (process.env.NODE_ENV === 'worker') {
+            clusterWorkerSize = parseInt(process.env.NUMBER_WORKERS);
+        } else {
+            // Usa 1/4 dos cores disponíveis OU no máximo 8 (o que for menor)
+            clusterWorkerSize = Math.min(Math.max(Math.floor(totalCPUs / 4), 1), 8);
         }
 
-        let worker = cluster.fork(ENV_VAR);
+        console.log(`Total CPUs: ${totalCPUs}, Workers: ${clusterWorkerSize}`);
+
+        for (let i = 0; i < clusterWorkerSize; i++) {
+            let ENV_VAR = {};
+            if (i === 0) {
+                ENV_VAR = { 'PRIMARY_WORKER': 1 };
+            }
+            cluster.fork(ENV_VAR);
+        }
+
+        cluster.on("exit", function(worker) {
+            console.log(process.env.APP_NAME, worker.id, "has exited.");
+        });
+    } else {
+        require('./app.js');
     }
-
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('worker ' + worker.process.pid + ' died');
-    });
-
 } else {
     require('./app.js');
 }
